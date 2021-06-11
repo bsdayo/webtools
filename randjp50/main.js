4425c200f99ef7a9bee2049764b94fb94a993961
BlackSoil <bs@blacksoil.top>
2021-06-11 20:27:29 +0800

    [randjp50] 增加模式选择

--- randjp50/main.js
+++ randjp50/main.js
@@ -3,37 +3,58 @@ const app = new Vue({
   data: {
     jpData,
     rowRange: ['あ行'],
+    genMode: 'kana2pron',
     count: 5,
-    allChars: [],
     kana: '平假',
     ifAnswersSeen: false,
-    practice: '点击按钮生成练习',
-    answers: ''
+    practice: '点击按钮生成练习'
   },
   methods: {
-    generate() {
+    gen() {
       this.practice = ''
       this.answers = ''
-      this.allChars = []
-      this.allAnswers = []
+      this.allQs = []
+      this.allAs = []
       this.ifAnswersSeen = false
 
-      for (let i of this.rowRange) {
-        for (let c of this.jpData[i]) {
-          if (this.kana == '平假') {
-            this.allChars.push(c['hara'])
-          }
-          if (this.kana == '片假') {
-            this.allChars.push(c['kata'])
-          }
-          this.allAnswers.push(c['pron'])
-        }
+      if (this.genMode == 'kana2pron') {
+        this.gen_kana2pron()
+      } else if (this.genMode == 'pron2kana') {
+        this.gen_pron2kana()
       }
 
       for (let n = 0; n < this.count; n++) {
-        let num = Math.floor(Math.random() * this.allChars.length)
-        this.practice += this.allChars[num] + ' '
-        this.answers += this.allAnswers[num] + ' '
+        let num = Math.floor(Math.random() * this.allQs.length)
+        this.practice += this.allQs[num] + ' '
+        this.answers += this.allAs[num] + ' '
+      }
+    },
+
+    gen_kana2pron() {
+      // 假名 ‣ 发音
+      for (let i of this.rowRange) {
+        for (let c of this.jpData[i]) {
+          if (this.kana == '平假') {
+            this.allQs.push(c['hara'])
+          } else if (this.kana == '片假') {
+            this.allQs.push(c['kata'])
+          }
+          this.allAs.push(c['pron'])
+        }
+      }
+    },
+
+    gen_pron2kana() {
+      // 发音 ‣ 假名
+      for (let i of this.rowRange) {
+        for (let c of this.jpData[i]) {
+          if (this.kana == '平假') {
+            this.allAs.push(c['hara'])
+          } else if (this.kana == '片假') {
+            this.allAs.push(c['kata'])
+          }
+          this.allQs.push(c['pron'])
+        }
       }
     }
   }
