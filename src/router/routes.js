3eca15b78d607f744f2d996f15c560ccff2fdb9e
b1acksoil <bs@blacksoil.top>
2021-10-02 02:05:15 +0800

    [core] init vue project

---
+++ src/router/routes.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    path: '/',
+    children: [
+      // Core
+      { path: 'home', component: () => import('../views/Home.vue') },
+
+      // Tools
+      { path: 'arcpurchase', component: () => import('../views/Home.vue') },
+      { path: 'randjp50', component: () => import('../views/Home.vue') },
+      { path: 'wordcount', component: () => import('../views/Home.vue') },
+    ]
+  },
+  {
+    path: '/:catchAll(.*)',
+    component: () => import('../views/About.vue')
+  }
+]
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+// import  from ''
+
+export default defineComponent({
+  name: '',
+  components: {  },
+  setup() {
+    return {
+      
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
