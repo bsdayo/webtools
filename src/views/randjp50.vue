13782b54b2aa96dc4e566cf94baebe7af058c5c9
b1acksoil <bs@blacksoil.top>
2021-10-03 03:37:40 +0800

    add empty randjp50 view

---
+++ src/views/randjp50.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="randjp50-view">
+    
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+// import  from ''
+
+export default defineComponent({
+  name: 'randjp50View',
+  components: {  },
+  setup() {
+    return {
+      
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
